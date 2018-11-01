77ed8b071b917fa60bd75aefc653709d086af13a
Antonio Mika <antoniomika@gmail.com>
2018-11-01 14:42:18 -0400

    Fixed port for gce tcp proxy

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+
 	"github.com/notion/trove_ssh_bastion/config"
 	"github.com/notion/trove_ssh_bastion/ssh"
 	"github.com/notion/trove_ssh_bastion/web"
-	"os"
-	"os/signal"
 )
 
 var (
 	webAddr      = flag.String("web.addr", ":8080", "The address to listen for http connections on")
-	sshAddr      = flag.String("ssh.addr", ":2222", "The address to listen for ssh connections on")
+	sshAddr      = flag.String("ssh.addr", ":5222", "The address to listen for ssh connections on")
 	sshProxyAddr = flag.String("ssh.proxy.addr", "localhost:22222", "The address to listen for ssh proxy connections on")
 	forceCerts   = flag.Bool("ssh.force-certs", false, "Force certificate generation")
 )
