e0247df2a4d7ae7588b9b6d84f790c5e67c2a875
Antonio Mika <antoniomika@gmail.com>
2019-01-20 18:40:20 -0500

    Update build steps to include static

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 rm -rf build/
 mkdir -p build/web
 cp $(which bastion) build/bastion
-cp -R web/templates build/web
+find web/ -maxdepth 1 -mindepth 1 -type d -exec cp -R {} build/web \;
 cp config.example.yml build/
 tar -czvf bastion.tar.gz --directory ./build .
